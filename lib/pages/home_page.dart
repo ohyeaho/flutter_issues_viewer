154f44a84bd1bcd553b6c9e390095f70783972ae
ohyeaho <ohyeah7275@gmail.com>
2022-04-01 01:54:50 +0900

    create sort dialog UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -102,9 +102,55 @@ class _HomePageState extends State<HomePage>
     return Align(
       alignment: Alignment.centerRight,
       child: IconButton(
-        onPressed: () {},
+        onPressed: () {
+          showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => alertDialog(context));
+        },
         icon: const Icon(Icons.filter_list),
       ),
+    );
+  }
+
+  AlertDialog alertDialog(BuildContext context) {
+    return AlertDialog(
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: const [
+            Text('1年以上更新のないIssueを除外する'),
+            Text('closed状態のIssueを除外する'),
+            Text('作成日時の新しい順'),
+            Text('最終更新日時の古い順'),
+            Text('コメント数の多い順'),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        Center(
+          child: SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: ElevatedButton(
+                child: const Text(
+                  '適用する',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[400],
+                  onPrimary: Colors.black,
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+          ),
+          // child: TextButton(
+          //   onPressed: () => Navigator.pop(context, 'OK'),
+          //   child: const Text('OK'),
+          // ),
+        ),
+      ],
     );
   }
 }
