9a1b9d3920dd72c3873ef69bef3f4e8b8b9788c6
ohyeaho <ohyeah7275@gmail.com>
2022-04-01 01:25:04 +0900

    create screen UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,17 +7,103 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  static const List<Tab> labels = <Tab>[
+    Tab(text: '全て'),
+    Tab(text: 'p: web view'),
+    Tab(text: 'p: shared_preferences'),
+    Tab(text: 'waiting for customer response'),
+    Tab(text: 'severe: new feature'),
+    Tab(text: 'p: share'),
+  ];
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: labels.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Issues Viewer'),
-      ),
-      body: const Center(
-        child: Text(
-          'You have pushed the button this many times:',
+        automaticallyImplyLeading: false,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(40),
+          child: _buildTabsRow(),
         ),
+      ),
+      body: _tabBody(),
+    );
+  }
+
+  /// タブ内容
+  Widget _tabBody() {
+    return TabBarView(
+      controller: _tabController,
+      children: labels.map((Tab tab) {
+        final String label = tab.text!.toLowerCase();
+        return ListView.builder(itemBuilder: (context, index) {
+          return Center(
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(label),
+              ),
+            ),
+          );
+        });
+      }).toList(),
+    );
+  }
+
+  /// ラベルとソートボタンの区画割り
+  Widget _buildTabsRow() {
+    return Stack(
+      children: [
+        _buildTabLabels(),
+        _buildTabActions(),
+      ],
+    );
+  }
+
+  /// ラベル
+  Widget _buildTabLabels() {
+    return Row(
+      children: [
+        Expanded(
+          flex: 9,
+          child: TabBar(
+            controller: _tabController,
+            tabs: labels,
+            isScrollable: true,
+          ),
+        ),
+        const Expanded(
+          flex: 2,
+          child: SizedBox(),
+        ),
+      ],
+    );
+  }
+
+  /// ソートボタン
+  Widget _buildTabActions() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.filter_list),
       ),
     );
   }
